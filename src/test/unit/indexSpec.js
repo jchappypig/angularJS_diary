b767cb845227f4786e834b78c47f44a7379bba11
Huanhuan Huang <hhuang@thoughtworks.com>
2014-02-18 15:34:25 +1100

    finnally get test working

---
+++ src/test/unit/indexSpec.js
@@ -0,0 +1,9 @@
+describe('unit test: CalculationController', function () {
+  'use strict';
+  describe('add function', function () {
+    it("should greater than eight", function () {
+      var value = 9;
+      expect(value).toBeGreaterThan(8);
+    });
+  });
+});
